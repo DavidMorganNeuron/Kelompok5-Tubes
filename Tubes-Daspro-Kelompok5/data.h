d1f6d42d0edfde7b694b95b8659db02b2d71fff3
DavidMorganNeuron <davidneuron21@gmail.com>
2025-12-02 23:19:15 +0700

    pesan commit

---
+++ Tubes-Daspro-Kelompok5/data.h
@@ -0,0 +1,9 @@
+// variabel global
+// index 0 = Nasi Goreng, index 1 = Ayam Geprek, index 2 = Ifumie Goreng, index 3 = Es Teh Manis, index 4 = Kubisu, index 5 = Air Mineral
+int harga[6] = {15000, 14000, 13000, 5000, 10000, 3000};
+int stok[6]  = {50, 40, 50, 100, 30, 200}; 
+
+// variabel laporan
+int totalPendapatan = 0;
+int totalPembelian[6] = {0, 0, 0, 0, 0, 0};
+int hargaSatuan[6] = {0, 0, 0, 0, 0, 0};
---
+++ Tubes-Daspro-Kelompok5/main.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define pass 123
+
+// file header
+#include "data.h" // variabel global
+#include "fitur_struk.h" // struk
+#include "fitur_transaksi.h" // transaksi
+#include "fitur_stok.h" // stok
+#include "fitur_laporan.h" // laporan
+
+// fitur level
+void menuAdmin() {
+    int menu;
+    do {
+        system("cls");
+        printf("==========================================\n");
+        printf("|             HALAMAN ADMIN              |\n");
+        printf("==========================================\n");
+        printf("|1. Laporan Penjualan                    |\n");
+        printf("|2. Tambah Stok Barang                   |\n");
+        printf("|3. Keluar (Logout)                      |\n");
+        printf("==========================================\n");
+        printf("[1/2/3]: ");
+        scanf("%d", &menu);
+
+        switch (menu) {
+            case 1: 
+                lihatLaporan(); // pindah ke fitur_laporan.h
+                break;
+            case 2: 
+                tambahStok(); // pindah ke fitur_stok.h
+                break;
+            case 3: 
+                printf("Logout berhasil.\n"); 
+                break;
+            default: 
+                printf("Coba lagi.\n"); 
+                system("pause"); // berfungsi sebagai jeda
+        }
+    } while (menu != 3);
+}
+
+void menuPegawai() {
+    int menu;
+    do {
+        system("cls");
+        printf("==========================================\n");
+        printf("|            HALAMAN PEGAWAI             |\n");
+        printf("==========================================\n");
+        printf("|1. Transaksi                            |\n");
+        printf("|2. Keluar (Logout)                      |\n");
+        printf("==========================================\n");
+        printf("[1/2]: ");
+        scanf("%d", &menu);
+
+        switch (menu) {
+            case 1: 
+                menuTransaksi(); // pindah ke fitur_tampil.h
+                break;
+            case 2: 
+                printf("Logout berhasil.\n"); 
+                break;
+            default: 
+                printf("Coba lagi.\n");
+                system("pause");
+        }
+    } while (menu != 2);
+}
+
+int main() {
+    int level;
+    int pw;
+    
+    do {
+        system("cls");
+        printf("==========================================\n");
+        printf("|              WARKOP CKCK               |\n");
+        printf("==========================================\n");
+        printf("|Login Sebagai:                          |\n");
+        printf("|1. Admin                                |\n");
+        printf("|2. Pegawai                              |\n");
+        printf("|3. Tutup Program                        |\n");
+        printf("==========================================\n");
+        printf("[1/2/3]: ");
+        scanf("%d", &level);
+
+        if (level == 1) {
+            // password
+            printf("Masukkan Password Anda: ");
+            scanf("%d", &pw);
+            
+            if (pw == pass) {
+                printf("Login Berhasil!\n");
+                system("pause");
+                menuAdmin();
+            } else {
+                printf("Password Salah! Akses Ditolak.\n");
+                system("pause");
+            }
+        } else if (level == 2) {
+            menuPegawai();
+        } else if (level == 3) {
+            system("cls");
+            printf("================================================\n");
+            printf("|\t\tPROGRAM SELESAI\t\t       |\n");
+            printf("| Terima Kasih Telah Berkunjung ke WARKOP CKCK |\n");
+            printf("================================================\n");
+        } else {
+            printf("Pilihan tidak valid.\n");
+            system("pause");
+        }
+    } while (level != 3);
+
+    return 0;
+}
